fabca938ce74fa49de8ee42d634c4eae6b7084bd
Ami <168814388+ami-diary@users.noreply.github.com>
2025-10-29 23:28:09 +0300

    Add files via upload

--- Lab5_RemoveEvenNumbers/Program.cs
+++ Lab5_RemoveEvenNumbers/Program.cs
@@ -1,1 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
 
+namespace Lab5_RemoveEvenNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите количество элементов массива: ");
+            int n = int.Parse(Console.ReadLine());
+
+            int[] array = new int[n];
+            Random rand = new Random();
+
+            Console.WriteLine("Исходный массив:");
+            for (int i = 0; i < n; i++)
+            {
+                array[i] = rand.Next(-10, 11);
+                Console.Write(array[i] + " ");
+            }
+
+            Console.WriteLine("\n");
+
+            // Удаляем чётные элементы
+            List<int> oddNumbers = new List<int>();
+            foreach (int num in array)
+            {
+                if (num % 2 != 0)
+                    oddNumbers.Add(num);
+            }
+
+            Console.WriteLine("Массив без чётных чисел:");
+            foreach (int num in oddNumbers)
+                Console.Write(num + " ");
+
+            Console.WriteLine();
+        }
+    }
+}
